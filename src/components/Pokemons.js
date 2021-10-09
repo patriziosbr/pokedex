794bbff1dead48dc7f9d318c38c2dc27d41b0750
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-10-09 10:18:29 +0200

    call axios ok

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,6 +1,19 @@
 import { Component } from "react";
+import axios from "axios";
+
 
 class Pokemons extends Component {
+    state= {
+        pokemons: [],
+    }
+    componentDidMount () {
+        axios.get("https://pokeapi.co/api/v2/pokemon")
+        .then( res => {
+            console.log(res);
+        }).catch( err => {
+            console.log('axios error', err);
+        })
+    }
     render(){
         return(
             <div>
