27b0e0402a7a84d748316c5f4be4b774858b6a56
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-10-10 16:31:41 +0200

    pokemon card from functional to class

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,30 +1,59 @@
 import {Link} from "react-router-dom";
 
-const PolemonCard = (props) => {
-    console.log(props);
+
+const PokemonCard = (props) => {
+    // console.log(props);
+
     return(
-        <div>
-            <div  className="card-box">
-                {props.pokemons.map((pokemon, key) => {
-                    return (
-                        <div className="card" key={key}>
-                            <Link
-                            to={{
-                                pathname: `pokemon/${pokemon.name}`
-                            }}>
-                                <div className="img-card">
-                                    <img src={`https://img.pokemondb.net/artwork/vector/large/${pokemon.name}.png`} alt={pokemon.name}/>
+            <div>
+                <div  className="card-box">
+                    {props.pokemons.map((pokemon, key) => {
+                        return (
+                            <div className="card" key={key}>
+                                <Link
+                                to={{
+                                    pathname: `pokemon/${pokemon.name}`
+                                }}>
+                                    <div className="img-card">
+                                        <img src={`https://img.pokemondb.net/artwork/vector/large/${pokemon.name}.png`} alt={pokemon.name}/>
+                                    </div>
+                                </Link>
+                                <div className="bottom">
+                                    <h4 className="text-black">{pokemon.name}</h4>
+                                    <form>
+                                        <div className="create-visible switch-field ">
+{/* 
+                                            <input 
+                                                type="radio"
+                                                value="si"
+                                                checked={captured === "si"}
+                                                onChange={handleChange}
+                                                id="visible-si"
+                                                className="form-check-input"  
+                                            />
+                                            <label  className="form-check-label not-strong yes " htmlFor="visible-si" onChange={handleChange}>Si</label>
+
+                                            <input 
+                                                type="radio"
+                                                value="no" 
+                                                checked={captured === "no"}
+                                                onChange={handleChange}
+                                                id="visible-no" 
+                                                className="form-check-input"
+                                            />
+                                            <label  className="form-check-label not-strong no " htmlFor="visible-no" onChange={handleChange} >No</label> */}
+
+
+                                        </div> 
+                                    </form>
                                 </div>
-                            </Link>
-                            <div className="bottom">
-                                <h4 className="text-black">{pokemon.name}</h4>
-                                <p>Captured toggle</p>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    )
-}
-export default PolemonCard;
+        )
+    }
+
+
+export default PokemonCard;
--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -13,7 +13,7 @@ class Pokemons extends Component {
         this.setState({loading : true})
         axios.get("https://pokeapi.co/api/v2/pokemon")
         .then( res => {
-            console.log(res.data.results); //all pokemons
+            // console.log(res.data.results); //all pokemons
         this.setState({pokemons : res.data.results, loading : false })       
         }).catch( err => {
             console.log('axios error', err);
